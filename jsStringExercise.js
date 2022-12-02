1ed7a47e70cca236f30c36e19739b07dea6f0ab5
TJP93 <tom.port@yahoo.co.uk>
2022-12-02 13:47:08 +0000

    better formatting, double checking Gitpod sync is really working also.

--- jsStringExercise.js
+++ jsStringExercise.js
@@ -1,12 +1,21 @@
-const sampleData = "Publication Date,Title,Authors 29/07/1954,Lord of the Rings,John Ronald Reuel Tolkien 01/08/1996,A Game of Thrones,George Raymond Martin 21/06/2003,Harry Potter and the Order of the Phoenix,Joanne Rowling"
+const sampleData = `Publication Date,Title,Authors
+29/07/1954,Lord of the Rings,John Ronald Reuel Tolkien
+01/08/1996,A Game of Thrones,George Raymond Martin
+21/06/2003,Harry Potter and the Order of the Phoenix,Joanne Rowling`
 
 // use characters to create table
 //string.split() to divide sample into parts?
 // padStart and padEnd for layout spacing?
 
 
-const date = Date.now();
-const dateString = date.toLocalDateString();
+const date = new Date();
+const dateString = date.toLocaleDateString('en-GB');
 const amount = 12.49;
  
 const display = `Spent £${amount} on ${dateString}`;
+
+console.log(display);
+
+let words = sampleData.split(' ');
+// console.log(words)
+let result = "";
